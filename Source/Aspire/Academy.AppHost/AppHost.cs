a3ebf1651918ba278bbcc52ffc461d088e69ff8a
David Tait <david@taitventures.com>
2025-08-10 09:35:15 +0100

    Integrate Vault for secret management and update configs
    - Added package references for Aspire.Hosting and VaultSharp in `Academy.AppHost.csproj`.
    - Modified `AppHost.cs` to use Vault for secret management, removing the previous Vault container implementation.
    - Updated `Extensions.cs` to fully comment out unused service discovery configuration.
    - Changed application URLs in `launchSettings.json` for local development.
    - Added `docker-compose.yaml` to define a Vault server service with necessary configurations.

--- Source/Aspire/Academy.AppHost/AppHost.cs
+++ Source/Aspire/Academy.AppHost/AppHost.cs
@@ -1,39 +1,65 @@
-using Aspire.Hosting;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.Commons;
 
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
+//-------------------------------------
+// Get the secrets from Hashicorp Vault
+//-------------------------------------
+
+// Used by Vault for segregation of credentials
+IResourceBuilder<ParameterResource> academyInstance = builder.AddParameter("academy-instance", "development", publishValueAsDefault: false, secret: true);
+
+// Default to using Vault which can be launched from the separate docker-compose.yaml file. You can override this by setting it in the user secrets.
+IResourceBuilder<ParameterResource> vaultUrl = builder.AddParameter("vault-url", "http://host.docker.internal:8200", publishValueAsDefault: false, secret: true);
 IResourceBuilder<ParameterResource> vaultToken = builder.AddParameter("vault-token", secret: true);
 
-IResourceBuilder<ContainerResource> vault = builder.AddContainer("vault", "hashicorp/vault:latest")
-        .WithEndpoint(8200, 8200, name: "vault-http")
-        .WithEnvironment("VAULT_DEV_ROOT_TOKEN_ID", vaultToken)
-        .WithEnvironment("VAULT_DEV_LISTEN_ADDRESS", "0.0.0.0:8200")
-//.WithBindMount("/vault/config", "./vault-config");
-//.WithHttpHealthCheck("http://localhost:8200/v1/sys/health", TimeSpan.FromSeconds(30));
-;
+// Initialize one of the several auth methods.
+IAuthMethodInfo authMethod = new TokenAuthMethodInfo(await vaultToken.Resource.GetValueAsync(default) ?? "");
 
-IResourceBuilder<ParameterResource> username = builder.AddParameter("db-username", secret: true);
-IResourceBuilder<ParameterResource> password = builder.AddParameter("db-password", secret: true);
+// Initialize settings. You can also set proxies, custom delegates etc. here.
+VaultClientSettings vaultClientSettings = new(await vaultUrl.Resource.GetValueAsync(default) ?? "", authMethod);
 
-IResourceBuilder<PostgresServerResource> postgres = builder.AddPostgres("postgres", username, password)
-                                                           .WithDataVolume(isReadOnly: false);
+VaultClient vaultClient = new(vaultClientSettings);
 
-IResourceBuilder<PostgresDatabaseResource> postgresdb = postgres.AddDatabase("Academy");
+// We have a KeyValue secrets engine called kv-v2 with a secret matching the academy-instance and containing key value pairs below
+Secret<SecretData> secret = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(await academyInstance.Resource.GetValueAsync(default));
+string dbUsernameSecret = ((System.Text.Json.JsonElement)secret.Data.Data["db-username"]).GetString() ?? "";
+string dbPasswordSecret = ((System.Text.Json.JsonElement)secret.Data.Data["db-password"]).GetString() ?? "";
 
+if (string.IsNullOrEmpty(dbUsernameSecret) || string.IsNullOrEmpty(dbPasswordSecret))
+{
+    throw new Exception("username or password cannot be null or empty");
+}
+
+// Inject our Postgres credentials
+IResourceBuilder<ParameterResource> username = builder.AddParameter("db-username", dbUsernameSecret, publishValueAsDefault: false, secret: true);
+IResourceBuilder<ParameterResource> password = builder.AddParameter("db-password", dbPasswordSecret, publishValueAsDefault: false, secret: true);
+
+// Add more secrets here as we require them
+
+//-------------------
+// Start the services
+//-------------------
+
+// Redis Cache
 IResourceBuilder<RedisResource> cache = builder.AddRedis("cache");
 
+// Postgres
+IResourceBuilder<PostgresServerResource> postgres = builder.AddPostgres("postgres", username, password)
+                                                           .WithDataVolume(isReadOnly: false);
+// Create the DB
+IResourceBuilder<PostgresDatabaseResource> postgresdb = postgres.AddDatabase("Academy");
+
+// API Service
 IResourceBuilder<ProjectResource> apiService = builder.AddProject<Projects.Academy_Services_Api>("services-api")
                                                       .WithReference(postgresdb)
                                                       .WithHttpHealthCheck("/health")
                                                       .WithReference(cache)
-                                                      .WaitFor(cache);
+                                                      .WaitFor(cache)
+                                                      ;
 
-//builder.AddProject<Projects.Academy_Web>("clients-web")
-//       .WithExternalHttpEndpoints()
-//       .WithHttpHealthCheck("/health")
-//       .WithReference(cache)
-//       .WaitFor(cache)
-//       .WithReference(apiService)
-//       .WaitFor(apiService);
-
+// Start it all
 builder.Build().Run();

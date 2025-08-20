670b40fae6fddfb6f92d46be1a580102d165a5e0
David Tait <david@taitventures.com>
2025-08-20 21:06:05 +0100

    Update metadata title in layout and refactor NextAuth provider configuration

--- Source/Clients/academy.clients.web/src/pages/api/auth/[...nextauth].ts
+++ Source/Clients/academy.clients.web/src/pages/api/auth/[...nextauth].ts
@@ -1,19 +1,18 @@
-import NextAuth from "next-auth";
-import { NextAuthOptions } from "next-auth";
-import FusionAuthProvider from "next-auth/providers/fusionauth";
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import FusionAuthProvider from 'next-auth/providers/fusionauth'
 
 export const authOptions: NextAuthOptions = {
   providers: [
-  FusionAuthProvider({
-    id: "fusionauth",
-    name: "FusionAuth",
-    issuer:  process.env.FUSIONAUTH_ISSUER,
-    clientId: process.env.FUSIONAUTH_CLIENT_ID,
-    clientSecret: process.env.FUSIONAUTH_SECRET,
-    tenantId: process.env.FUSIONAUTH_TENANT_ID // Only required if you're using multi-tenancy
-  }),
+    FusionAuthProvider({
+      id: 'fusionauth',
+      name: 'FusionAuth',
+      issuer: process.env.FUSIONAUTH_ISSUER,
+      clientId: process.env.FUSIONAUTH_CLIENT_ID,
+      clientSecret: process.env.FUSIONAUTH_SECRET,
+      tenantId: process.env.FUSIONAUTH_TENANT_ID, // Only required if you're using multi-tenancy
+    }),
   ],
   // Add more options as needed
-};
+}
 
-export default NextAuth(authOptions);
+export default NextAuth(authOptions)
